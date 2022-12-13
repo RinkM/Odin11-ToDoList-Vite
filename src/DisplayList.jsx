54e2d305877dfe337ac30f74e8af6888ef126378
RinkM <mikerinkert@gmail.com>
2022-12-12 17:34:00 -0700

    major update. adds most details in task

--- src/DisplayList.jsx
+++ src/DisplayList.jsx
@@ -8,31 +8,77 @@ const DisplayList = (props) =>{
   const [state, setState] = useState()
   const [library, setLibrary] = props.hookLibrary
 
-  
-
 
   const markComplete = (reactKey)=> {
+
+    const removeTask = document.getElementById("container"+reactKey)
+    removeTask.classList.add("removeTaskAnimation")
+
     const index = toDoLibrary.findIndex(item =>{
       return item.reactKey == reactKey
     })
-    const removeTask = document.getElementById(reactKey)
-    removeTask.classList.add("removeTaskAnimation")
-
 
     toDoLibrary.splice(index,1)
     setTimeout(()=>setLibrary([...toDoLibrary]), 1000)
-    
-
-
   }
 
+  const viewDetails = (reactKey)=> {
+    const taskContainer = document.getElementById("container"+reactKey)
+    taskContainer.classList.toggle("showDetails")
+
+    const taskBody = document.getElementById("body"+reactKey)
+    taskBody.classList.toggle("hidden")
+  }
+
+  
 
   const ListItem =  (props)=> {
+
+    const priority = ()=>{
+      if (props.item.priority == 3){
+        return (<span className="spanPriority"> Priority: 
+          <i style ={{color: "red"}} className="priority fa fa-exclamation-circle fa-lg" aria-hidden="true">
+            </i>
+            High</span>
+        )
+      } else if (props.item.priority == 2){
+        return (<span className="spanPriority">Priority: 
+          <i style = {{color: "orange"}} className="priority fa fa-exclamation-circle fa-lg" aria-hidden="true"></i>Medium</span>
+        )
+      } else if (props.item.priority == 1){
+        return (<span className="spanPriority">Priority: 
+          <i style = {{color: "green"}}className="priority fa fa-exclamation-circle fa-lg" aria-hidden="true"></i>Low</span>
+        )
+      } else if (props.item.priority == null){
+        return 
+      }
+    }
+
     return (
-    <div className = "toDoItem" id = {props.item.reactKey}>
-    {props.item.toDoName} 
-    <button onClick={()=>markComplete(props.item.reactKey)} className="finishedBtn">Finished</button>
-  </div>
+    <div className = "taskContainer" id = {"container"+props.item.reactKey}>
+      <div className = "taskHeader" id = {"header"+props.item.reactKey}>
+        {props.item.toDoName} 
+        <div className="taskButtons">
+          <button onClick={()=>viewDetails(props.item.reactKey)} className="detailsBtn">
+          <i className="fa fa-list" aria-hidden="true"></i>
+          </button>
+          <button onClick={()=>markComplete(props.item.reactKey)} className="finishedBtn">
+            <i className="fa fa-check fa-lg" aria-hidden="true"></i>
+          </button>
+        </div>
+      </div>
+      <div id ={"body"+props.item.reactKey} className = {"taskBody hidden"}>
+       
+       {/* ! here is where all I was last working. Adds the details to the tasks. 
+       I feel like each thing (or most) should be a separate function.  */}
+       
+       <div>{priority()}</div>
+       <div>{props.item.description}</div>
+       <div>Project: {props.item.project}</div>
+       <div>{props.item.dueDate}</div>
+
+      </div>
+    </div>
   )}
 
 
--- src/toDoLibrary.jsx
+++ src/toDoLibrary.jsx
@@ -4,20 +4,42 @@
 const toDoLibrary = [
     {toDoName:"Eat Breakfast",
     completed:false,
-    reactKey:0},
+    reactKey:0,
+    description:"Eat eggs and toast.",
+    dueDate: 1680872994363,
+    priority: "3",
+    project:"Food",
+},
 
     {toDoName:"Eat Lunch",
     completed:false,
-    reactKey:1},
+    reactKey:1,
+    description:"Eat sandwich.",
+    dueDate: 1680872994363,
+    priority: "2",
+    project:"Meals",
+    
+},
 
     {toDoName:"Eat Dinner",
     completed:false,
-    reactKey:2},
+    reactKey:2,
+    description:"Eat mac & cheese.",
+    dueDate: 1680872994363,
+    priority: "1",
+    project:"Food",
+},
 
     {toDoName:"Eat Snack",
     completed:false,
-    reactKey:3},
+    reactKey:3,
+    description:null,
+    dueDate: null,
+    priority: null,
+    project:"Food",
+},
 
 ]
 
 export default toDoLibrary
+
