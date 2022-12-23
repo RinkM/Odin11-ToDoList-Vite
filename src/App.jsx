23b79f00fb74f2cb0492e220f597dbfdb881160a
RinkM <mikerinkert@gmail.com>
2022-12-23 00:11:32 -0700

    active project hooks

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
-import AddForm from './addForm'
+import FormAddTask from './FormAddTask'
 import DisplayList from './DisplayList'
 import toDoLibrary from './toDoLibrary'
 import DisplayProjects from './DisplayProjects'
@@ -15,16 +15,16 @@ import { formatDistanceToNow, differenceInDays, differenceInMilliseconds  } from
 
 function App() {
   const [library, setLibrary] = useState(toDoLibrary)
-
+  const [activeProject, setActiveProject]= useState("All")
   return (
     <div className="App backgroundApp">
       <div className='appHeader backgroundApp'><h1>Get Stuff Done!</h1>
        
       </div>
-      <div className='navLeft flexWrapper backgroundApp'>
-        <DisplayProjects/>
+      <div className='navLeft flexWrapper'>
+        <DisplayProjects hookActiveProject = {[activeProject, setActiveProject]}/>
 
-        <AddForm hookLibrary = {[library, setLibrary]}/>
+        <FormAddTask hookActiveProject = {[activeProject, setActiveProject]} hookLibrary = {[library, setLibrary]}/>
       </div>
       <DisplayList hookLibrary = {[library, setLibrary]}/>
     </div>
