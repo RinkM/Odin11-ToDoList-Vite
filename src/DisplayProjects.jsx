e6fbfae6e06bed1d8264a0c2ce0f43893e7de413
RinkM <mikerinkert@gmail.com>
2022-12-23 00:12:05 -0700

    adds button / color / hook functions

--- src/DisplayProjects.jsx
+++ src/DisplayProjects.jsx
@@ -1,6 +1,6 @@
 
 import { useState } from "react";
-import ProjectForm from "./ProjectForm";
+import FormProject from "./FormProject";
 import projectLibrary from "./projectLibrary";
 
 
@@ -8,28 +8,29 @@ import projectLibrary from "./projectLibrary";
 
 
 
-const DisplayProjects = ()=>{
-  const [activeProject, setActiveProject]= useState("All")
-  const [projects, setProjects] = useState(projectLibrary)
+const DisplayProjects = (props)=>{
 
+  const [activeProject, setActiveProject] = props.hookActiveProject
+  const [projects, setProjects] = useState(projectLibrary)
+  
 
   const listProjects = projects.map((project) =>
-    <button id = {project.reactKey} className = "projectListItem" key={project.reactKey}
-      onClick= {()=>projectClick(project.name)}>     
-      {project.name}
+    <button id = {project.reactKeyProject} style ={{color: project.projectColor}} className = "projectListItem" key={project.reactKey}
+      onClick= {()=>projectClick(project)}>     
+      {project.projectName}
     </button>
 );
 
   const projectClick = (name)=>{
     setActiveProject(name)
-    console.log(activeProject)
+    console.log("active-Project", activeProject)
 
   };
 
 return(
   <div id="projectContainer backgroundForm">
 
-    <ProjectForm hookProject = {[projects, setProjects]} />
+    <FormProject hookProject = {[projects, setProjects]} hookActiveProject = {[activeProject, setActiveProject]} />
     <div className="projectList">{listProjects}</div>
 
   </div>
