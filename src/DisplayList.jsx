487ada793d2937b32aaff92b7219ac31695699d6
RinkM <mikerinkert@gmail.com>
2023-01-01 23:17:11 -0700

    fixes daySort

--- src/DisplayList.jsx
+++ src/DisplayList.jsx
@@ -29,10 +29,11 @@ const DisplayList = (props) =>{
     const daySort = () => {
       const today = Date.now();
       const days = differenceInCalendarDays(props.item.dueDate, today);
-      // console.log("daystildue", days)
-      if (activeProject.time == "Any Time"){
+      console.log("daystildue", days)
+      console.log("activeprojcect time", deadlineFilter)
+      if (deadlineFilter.filter == "Any Time"){
         return true
-      } else if (days >= activeProject.time) {
+      } else if (days >= deadlineFilter.filter) {
         return false
       } else {
         return true
@@ -52,7 +53,10 @@ const DisplayList = (props) =>{
     }
 
     if (daySort() == true && projectSort() ==true){
-      return <TaskHeader hookActiveProject = {[activeProject, setActiveProject]} hookDeadlineFilter = {[deadlineFilter, setDeadlineFilter]} props = {props.item} hookLibrary = {[library, setLibrary]}/>
+      return <TaskHeader 
+      hookActiveProject = {[activeProject, setActiveProject]} 
+      hookDeadlineFilter = {[deadlineFilter, setDeadlineFilter]} 
+      props = {props.item} hookLibrary = {[library, setLibrary]}/>
     } else if (daySort() == false && projectSort() ==true){
 
 
@@ -66,12 +70,16 @@ console.log("deadlineFilter", deadlineFilter)
         <h2>{activeProject.projectName} Projects Due: {deadlineFilter.listText}</h2>
       </div>
       {library.map((item)=>
-        <SortItem key ={item.reactKey+Date.now()} item = {item}/>
+        <SortItem 
+        key ={item.reactKey+Date.now()} 
+        item = {item}/>
         )}
 
       <div id = "taskFormDiv" className="taskFormContainer backgroundForm">
         <AddTaskBtn/>
-        <FormAddTask hookActiveProject = {[activeProject, setActiveProject]} hookLibrary = {[library, setLibrary]}/>
+        <FormAddTask 
+        hookActiveProject = {[activeProject, setActiveProject]} 
+        hookLibrary = {[library, setLibrary]}/>
       </div>
     </div>
       
